1ae3e797d1d0a2dcc605a56ed80eb70a99a303a4
Don Locke <donaldrlocke@gmail.com>
2024-11-14 16:57:35 -0700

    Remove incorrect port

--- ct/wavelog.sh
+++ ct/wavelog.sh
@@ -95,4 +95,4 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}:8086${CL} \n"
+         ${BL}http://${IP}${CL} \n"
