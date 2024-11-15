78c731f98cc214f6f2a7613ea741d287e8ef66d8
Don Locke <donaldrlocke@gmail.com>
2024-11-14 17:30:30 -0700

    update url for testing

--- ct/wavelog.sh
+++ ct/wavelog.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/DonLocke/ProxmoxVE/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/DonLocke/ProxmoxVE/add-wavelog-script-testing/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: Don Locke (DonLocke)
 # License: MIT
