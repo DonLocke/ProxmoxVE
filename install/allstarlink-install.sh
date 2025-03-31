c00c1cd20c6325274cf31e951de5796e5b1a346a
DonLocke <donrlocke@gmail.com>
2025-03-31 11:45:46 -0600

    update script

--- install/allstarlink-install.sh
+++ install/allstarlink-install.sh
@@ -23,11 +23,33 @@ msg_info "Installing AllStarLink"
 $STD apt-get install -y asl3
 msg_ok "Installed AllStarLink"
 
+msg_info "Configuring AllStarLink"
+SERVER_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c13)
+sed -i "/secret /s= .*/= $SERVER_PASS/" /etc/asterisk/manager.conf
+msg_ok "Configured AllStarLink"
+
+read -r -p "Would you like to set up AllStarLink Node now? <y/N> " prompt
+if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+  node-setup
+else
+  msg_warn "You will need to run \`node-setup\` before you can connect to the AllStarLink Network."
+fi
+
 read -r -p "Would you like to add Allmon3? <y/N> " prompt
 if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
   msg_info "Installing Allmon3"
   $STD apt-get install -y allmon3
   msg_ok "Installed Allmon3"
+
+  NODE=$(grep -oP '^\[\d+\]\(node-main\)' /etc/asterisk/rpt.conf | grep -oP '\d+')
+  if [[ -n $NODE ]]; then
+    msg_info "Configuring Allmon3"
+    sed -i "s/;[1999]/[$NODE]/" /etc/allmon3/allmon3.ini
+    sed -i "s/;host/host/" /etc/allmon3/allmon3.ini
+    sed -i "s/;user/user/" /etc/allmon3/allmon3.ini
+    sed -i "s/;pass=.*/pass=$SERVER_PASS" /etc/allmon3/allmon3.ini
+    msg_ok "Configured Allmon3"
+  fi
 fi
 
 motd_ssh
